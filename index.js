4cc9be442e3dd6d6f7f6157dc207263d91020e85
Stacy <salyiela@icloud.com>
2024-03-31 16:32:32 +0300

    Completed lab assignment

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+
+function destructivelyAppendCat(){
+    cats.push("Ralph");
+}
+
+function destructivelyPrependCat(){
+    cats.unshift("Bob")
+}
+
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+
+
+function appendCat(){
+    const newAppend = [...cats, "Broom"]
+    return newAppend
+}
+
+function prependCat(){
+    const newPrepend = ["Arnold", ...cats]
+    return newPrepend
+}
+
+function removeLastCat(){
+    const byeCat = cats.slice(0, 2)
+    return byeCat
+}
+
+function removeFirstCat(){
+    const byeFirstCat = cats.slice(1, 3)
+    return byeFirstCat
+}
